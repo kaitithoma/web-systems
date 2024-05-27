1036e2022036dce143f97173692fbb8c3f2dd8b2
Kaiti Thoma <kthoma@convertgroup.com>
2024-05-27 20:05:44 +0300

    refactor: Add PgSearch for improved search functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :brand, optional: true
+  belongs_to :category, optional: true
   has_many :retailer_products
   has_many :product_price_metrics, through: :retailer_products
 
-  delegate :name, to: :brand, prefix: true
+  delegate :name, to: :brand, prefix: true, allow_nil: true
+  delegate :name, to: :category, prefix: true, allow_nil: true
+
+  pg_search_scope :search_text, against: %i[name aliases], ignoring: :accents,
+                                using: { tsearch: { prefix: true, tsvector_column: 'searchable' } }
+
+  def prices_per_site
+    retailer_products.map(&:last_price)
+  end
+
+  def lowest_price_history
+    product_price_metrics.order(:price).pluck(:price)
+  end
 end
---
+++ config/initializers/pg_search.rb
@@ -0,0 +1,1 @@
+PgSearch.unaccent_function = 'custom_unaccent'
