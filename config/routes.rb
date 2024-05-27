6936f67ecdda81760fbdf4758c893a7c9896849e
Kaiti Thoma <kthoma@convertgroup.com>
2024-05-27 20:06:58 +0300

    refactor: Update routes.rb for product and site endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'products/:id' => 'products#show'
+  get 'search' => 'products#search', as: :search
+
+  get 'products/:id/price_history' => 'products#price_history',
+      as: :products_price_history
+
+  get 'sites' => 'sites#index', as: :sites
+  get 'sites/:id' => 'sites#show', as: :site
+  patch 'sites/:id' => 'sites#update', as: 'edit_site'
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'products#index'
+
+  resources :products, only: %i[index show update destroy]
 end
